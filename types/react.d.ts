c9dc59ab0790897b87145bfef260dafacd85170f
Ivan Bobchenkov <44039577+MeloncholyShiro@users.noreply.github.com>
2022-09-19 05:26:14 +0300

    fix: wrong types
    Without this, you have to disable the types that come with the library, and write exactly the same

--- types/react.d.ts
+++ types/react.d.ts
@@ -1,10 +1,21 @@
-declare module 'virtual:icons/*' {
-  import type { SVGProps } from 'react'
-  import type React from 'react'
-  const component: (props: SVGProps<SVGSVGElement>) => React.ReactElement
-  export default component
+declare module '~icons/*?raw' {
+  const component: string;
+  export default component;
 }
+
+declare module 'virtual:icons/*?raw' {
+  const component: string;
+  export default component;
+}
+
 declare module '~icons/*' {
+  import type React from 'react';
+  import type { SVGProps } from 'react';
+  const component: (props: SVGProps<SVGSVGElement>) => React.ReactElement;
+  export default component;
+}
+
+declare module 'virtual:icons/*' {
   import type { SVGProps } from 'react'
   import type React from 'react'
   const component: (props: SVGProps<SVGSVGElement>) => React.ReactElement
